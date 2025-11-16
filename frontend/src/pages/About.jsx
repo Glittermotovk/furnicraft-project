872ccef03eaa0b0c5795a227a81f9994dcd9dc04
Yuvrajsinh Chauhan <yuvrajsinhchauhan047@gmail.com>
2025-11-16 16:59:24 +0530

    Add About page for FurniCraft

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function About(){
+  return (
+    <div className="container">
+      <h2>About FurniCraft</h2>
+      <p>FurniCraft makes handpicked furniture blends of style and comfort.</p>
+    </div>
+  )
+}
